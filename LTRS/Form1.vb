ec67884253a289d863a66f46d3bb353443f0db72
EpicPrivacy <47374573+EpicPrivacy@users.noreply.github.com>
2023-04-30 23:11:29 +0800

    Revert "Revert "Add project files.""
    This reverts commit feab494139155bf5bf686d92fe909943e97d541d.

---
+++ LTRS/Form1.vb
@@ -0,0 +1,26 @@
+﻿Imports Microsoft.Win32
+
+Public Class Form1
+
+
+    Private Sub Guna2Button1_Click(sender As Object, e As EventArgs) Handles Guna2Button1.Click
+
+    End Sub
+
+    Private Sub Guna2Button3_Click(sender As Object, e As EventArgs) Handles Guna2Button3.Click
+        Me.Hide()
+        Dim Form1 As New CPass
+        Form1.ShowDialog()
+    End Sub
+
+    Private Sub Guna2Button4_Click(sender As Object, e As EventArgs) Handles Guna2Button4.Click
+        Me.Hide()
+        Dim Form1 As New Register
+        Form1.ShowDialog()
+    End Sub
+
+    Private Sub Guna2ControlBox1_Click(sender As Object, e As EventArgs) Handles Guna2ControlBox1.Click
+        Me.Close()
+        Application.Exit()
+    End Sub
+End Class
