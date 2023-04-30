feab494139155bf5bf686d92fe909943e97d541d
EpicPrivacy <47374573+EpicPrivacy@users.noreply.github.com>
2023-04-30 22:33:40 +0800

    Revert "Add project files."
    This reverts commit 2ff66b3f25fa1570c64e6415a7e44be178507f11.

--- LTRS/CPass.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class CPass
-    Private Sub Guna2ControlBox3_Click(sender As Object, e As EventArgs) Handles Guna2ControlBox3.Click
-
-        Me.Close()
-        Application.Exit()
-    End Sub
-End Class
--- LTRS/Register.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Register
-    Private Sub Guna2ControlBox1_Click(sender As Object, e As EventArgs) Handles Guna2ControlBox1.Click
-        Me.Close()
-        Application.Exit()
-    End Sub
-End Class
